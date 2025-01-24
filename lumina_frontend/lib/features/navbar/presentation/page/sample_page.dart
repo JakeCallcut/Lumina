d088470e6a8b9ed5556cad027ac053806953c5e8
Joshua Wagar <wagarlaptop@gmail.com>
2025-01-24 11:05:03 +0000

    Added Column

--- lumina_frontend/lib/features/navbar/presentation/page/sample_page.dart
+++ lumina_frontend/lib/features/navbar/presentation/page/sample_page.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:lumina_frontend/core/themes/main_theme.dart';
+//import 'package:lumina_frontend/features/navbar/presentation/page/navbar.dart';
 
 class statsPage extends StatelessWidget {
+
+  String _title = "Usage and Stats";
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Stats Page"),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+       Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset("/images/logo64.png"),
+            ),
+            Text(_title, /*style: MainTheme.h1,*/),
+          ],
+        ),
+      ],
     );
   }
 }
