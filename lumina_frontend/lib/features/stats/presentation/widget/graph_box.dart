649781c21d1edfd344847d9bea056c7d2c23350b
Joshua Wagar <wagarlaptop@gmail.com>
2025-01-30 19:21:08 +0000

    Update graph_box.dart
    Graph with sample data

--- lumina_frontend/lib/features/stats/presentation/widget/graph_box.dart
+++ lumina_frontend/lib/features/stats/presentation/widget/graph_box.dart
@@ -10,35 +10,39 @@ class GraphBox extends StatelessWidget{
   Widget build(BuildContext context) {
     return Column(
       children: [
-        SizedBox(
-          height: 268,
-          width: 500,
-          child: LineChart(
-            LineChartData(
-              lineBarsData: [
-                LineChartBarData(
-                  spots: [
-                    FlSpot(0, 0),
-                    FlSpot(1, 1),
-                    FlSpot(2, 2),
-                    FlSpot(3, 3),
-                    FlSpot(4, 4),
-                    FlSpot(5, 5),
-                    FlSpot(6, 6),
-                    FlSpot(7, 7),
-                    FlSpot(8, 8),
-                    FlSpot(9, 9),
-                    FlSpot(10, 10),
-                  ],
-                  isCurved: true,
-                  color: MainTheme.luminaBlue,
-                  barWidth: 4,
-                  isStrokeCapRound: true,
-                  belowBarData: BarAreaData(show: false),
-                ),
-              ]
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(32.0),
+              child: LineChart(
+              LineChartData(
+                gridData: const FlGridData(show: true),
+                titlesData: const FlTitlesData(show: false),
+                borderData: FlBorderData(show: true),
+                lineBarsData: [
+                  LineChartBarData(
+                    spots: [
+                      FlSpot(0, 0),
+                      FlSpot(1, 1),
+                      FlSpot(2, 2),
+                      FlSpot(3, 3),
+                      FlSpot(4, 4),
+                      FlSpot(5, 5),
+                      FlSpot(6, 6),
+                      FlSpot(7, 7),
+                      FlSpot(8, 8),
+                      FlSpot(9, 9),
+                      FlSpot(10, 10),
+                    ],
+                    isCurved: true,
+                    color: MainTheme.luminaLightGreen,
+                    barWidth: 4,
+                    isStrokeCapRound: true,
+                    belowBarData: BarAreaData(show: false),
+                  ),
+                ]
+              ),
             ),
-          ),
+          ) 
         ),
       ],
     );
