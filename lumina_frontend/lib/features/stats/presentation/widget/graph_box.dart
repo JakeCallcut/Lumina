b41241c1616f8fee5cba58655931035e18267002
TheRealWhiteWyvern <wagarlaptop@gmail.com>
2025-01-30 10:51:22 +0000

    Update graph_box.dart

---
+++ lumina_frontend/lib/features/stats/presentation/widget/graph_box.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+
+class GraphBox extends StatelessWidget{
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}
