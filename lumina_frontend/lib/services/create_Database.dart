03db87bd54db759313cb3f225eccf678d51bdfcf
ChrisGarvie1254 <144692922+ChrisGarvie1254@users.noreply.github.com>
2025-02-10 18:04:59 +0000

    Started to create the database structure

--- lumina_frontend/lib/services/create_Database.dart
+++ lumina_frontend/lib/services/create_Database.dart
@@ -3,7 +3,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class Create {
   var db = FirebaseFirestore.instance;
 
-  void addTopLevelCollections() {
-    
+  void addCollections() {
+    final setupDoc = <String, String> {
+      "setup" : "done"
+    };
+    db.collection("Residents").doc("SampleUser").set(setupDoc).onError((e, _) => print ("error $e"));
+    db.collection("Residents").doc("").set(setupDoc).onError((e, _) => print ("error $e"));
+    db.collection("Homeowner").doc("").set(setupDoc).onError((e, _) => print ("error $e"));
+    db.collection("Energy Usage").doc("").set(setupDoc).onError((e, _) => print ("error $e"));
   }
 }
