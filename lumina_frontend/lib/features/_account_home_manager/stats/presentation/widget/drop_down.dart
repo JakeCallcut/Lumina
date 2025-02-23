d6aed41fbd25289a93ebb30155c90d8719e1777d
Joshua Wagar <wagarlaptop@gmail.com>
2025-02-23 22:09:22 +0000

    Column Resize

--- lumina_frontend/lib/features/_account_home_manager/stats/presentation/widget/drop_down.dart
+++ lumina_frontend/lib/features/_account_home_manager/stats/presentation/widget/drop_down.dart
@@ -17,42 +17,47 @@ class DropDown extends StatelessWidget {
           borderRadius: BorderRadius.circular(10), // Rounded edges        ],
         ),
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        'Your Homes',
-                        style: MainTheme.h1Black,
+            Expanded(
+              flex: 85, // Take up 85% of the space
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'Your Homes',
+                          style: MainTheme.h1Black,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: NameBox(name: '1 Lumina Care'),
+                  ),
+                ],
+              ),
+            ),
+            const Expanded(
+              flex: 15, // Take up 15% of the space
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: 10,
+                    height: 100,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: Colors.black,
                       ),
                     ),
-                  ],
-                ),
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: NameBox(name: '1 Lumina Care'),
-                ),
-              ],
-            ),
-            const Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: 10,
-                  height: 100,
-                  child: DecoratedBox(
-                    decoration: BoxDecoration(
-                      color: Colors.black,
-                    ),
                   ),
-                ),
-              ],
-            )
+                ],
+              ),
+            ),
           ],
         ),
       ),
--- lumina_frontend/lib/features/_account_home_manager/stats/presentation/widget/name_box.dart
+++ lumina_frontend/lib/features/_account_home_manager/stats/presentation/widget/name_box.dart
@@ -8,23 +8,25 @@ class NameBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(8.0),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10), // Rounded edges
-        boxShadow: const [
-          BoxShadow(
-            color: Colors.grey,
-            spreadRadius: 2,
-            blurRadius: 4,
-            offset: Offset(0, 2),
-          ),
-        ],
-      ),
-      child: Text(
-        name,
-        style: MainTheme.h1Black,
+    return Expanded(
+      child: Container(
+        padding: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10), // Rounded edges
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.grey,
+              spreadRadius: 2,
+              blurRadius: 4,
+              offset: Offset(0, 2),
+            ),
+          ],
+        ),
+        child: Text(
+          name,
+          style: MainTheme.h1Black,
+        ),
       ),
     );
   }
