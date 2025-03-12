70ef52cdd60ecdc8d726fa3f0792e67cff08c386
Joshua Wagar <145107165+TheRealWhiteWyvern@users.noreply.github.com>
2025-03-12 23:03:06 +0000

    Progress bar

--- lumina_frontend/lib/features/_account_home_manager/register/presentation/page/register_step_5.dart
+++ lumina_frontend/lib/features/_account_home_manager/register/presentation/page/register_step_5.dart
@@ -49,7 +49,7 @@ class _RegisterStep5State extends State<ManagerRegisterStep5> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Image.asset('assets/images/register_progress/progress_1.png'),
+                Image.asset('assets/images/register_progress/progress_5.png'),
                 Image.asset('assets/images/text_logo.png'),
                 const SizedBox(height: 20),
                 Text('Tell us more about you', style: MainTheme.h2White),
