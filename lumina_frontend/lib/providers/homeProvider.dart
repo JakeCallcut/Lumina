9b56d423768d0a31c63b447a2fde8c72b4bd095e
Joshua Wagar <145107165+TheRealWhiteWyvern@users.noreply.github.com>
2025-03-14 12:13:55 +0000

    user ID
    user id is now passed into provider

--- lumina_frontend/lib/providers/homeProvider.dart
+++ lumina_frontend/lib/providers/homeProvider.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:lumina_frontend/services/integration_Funcs.dart';
 
 class homeProvider extends ChangeNotifier {
+
+  static String uid = '';
+
+  static void setUid(String dynamicUid) {
+    uid = dynamicUid;
+  }
+
   Future<void> fetchData() async {
     // Add your data fetching logic here
+    var instance = Integration();
+    print(uid);
     notifyListeners();
   }
 }
