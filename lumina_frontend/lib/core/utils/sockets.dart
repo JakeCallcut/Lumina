11b79e2f7a81b3a4e9f88eb416964f6b7df86110
jcb-drkn <jcormd.jd@gmail.com>
2025-03-14 15:52:40 +0000

    im praying rn

--- lumina_frontend/lib/core/utils/sockets.dart
+++ lumina_frontend/lib/core/utils/sockets.dart
@@ -19,7 +19,7 @@ class Sockets with ChangeNotifier{
 
   Future<void> initSocket() async { //initialises the socket connection
     socket = io(
-      'ws://192.168.140.94:8080', // Use secure WebSocket with port
+      'wss://192.168.140.94:8080', // Use secure WebSocket with port
       OptionBuilder()
         .setTransports(['websocket'])
         .disableAutoConnect()
